803fa4f4d4593cf1f482cc6f8c6deb4affbabf78
unknown <olivianorinee@gmail.com>
2025-01-17 13:41:04 +0700

    push to repo

--- Tugas1-Olivia.py
+++ Tugas1-Olivia.py
@@ -1,9 +1,18 @@
-secret_word:"oli"
-ans:["_","_","_"]
-while 
-    guess = input("Guess a letter: ")
-    if guess in secret_word:
-
-    
-else:
-    print("try again")
+secret_word = "slay"
+ans = (["_"] * len(secret_word))
+while True:
+    print("answer:"," ".join(ans))
+    x = input("Enter a letter: ").lower()
+    if not x.isalpha():
+        print("invalid letter")
+        continue
+    if x in secret_word:
+        for i in range(len(secret_word)):
+            if secret_word[i] == x:       
+                ans[i] = x                
+        print("Good job")
+        if "_" not in ans:
+            print('Congratulations! You complete the task','"',secret_word,'"')
+            break
+    else:
+        print("Wrong guess.Try again")
