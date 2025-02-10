f580e5d78da4a91bd61e7e7cc8fd582dd1aa308e
unknown <olivianorinee@gmail.com>
2025-02-10 09:26:30 +0700

    push to repo

--- Tugas 3- Olivia.py
+++ Tugas 3- Olivia.py
@@ -1,35 +1,36 @@
-secret_word = "slay"
 def main(secret_word):
     ans = (["_"] * len(secret_word))
     list=[]
     attempt=6
-    while True:
-        print("answer:"," ".join(ans))
-        x = input("Enter a letter: ").lower()
-        if x in list:
-            print("Still have ",attempt,"attempt")
-            continue
-        else:
-            attempt=attempt-1
-            print("have",attempt,"attempt")
-        if attempt<=0:
-            print("done")
-            print('You lose')
-            break
-        list.append(x)
-        print(list)
-        if not x.isalpha():
-            print("invalid letter")
-            continue
-        if x in secret_word:
-            for i in range(len(secret_word)):
-                if secret_word[i] == x:       
-                    ans[i] = x 
-            print("Good job")
-            if "_" not in ans:
-                print('Congratulations! You complete the task','"',secret_word,'"')
-                print('You win')
+    def updatetext(attempt,ans,secret_word,list):
+        while True:
+            print("answer:"," ".join(ans))
+            x = input("Enter a letter: ").lower()
+            if x in list:
+                print("Still have ",attempt,"attempt")
+                continue
+            else:
+                attempt=attempt-1
+                print("have",attempt,"attempt")
+            if attempt<=0:
+                print("done")
+                print('You lose')
                 break
-        else:
-            print("Wrong guess.Try again")
-main(secret_word)
+            list.append(x)
+            print(list)
+            if not x.isalpha():
+                print("invalid letter")
+                continue
+            if x in secret_word:
+                for i in range(len(secret_word)):
+                    if secret_word[i] == x:       
+                        ans[i] = x 
+                print("Good job")
+                if "_" not in ans:
+                    print('Congratulations! You complete the task','"',secret_word,'"')
+                    print('You win')
+                    break
+            else:
+                print("Wrong guess.Try again")
+    updatetext(attempt,ans,secret_word,list)
+main('slay')
--- Tugas2-Olivia.py
+++ Tugas2-Olivia.py
@@ -1,35 +1,33 @@
 secret_word = "slay"
-def main(secret_word):
-    ans = (["_"] * len(secret_word))
-    list=[]
-    attempt=6
-    while True:
-        print("answer:"," ".join(ans))
-        x = input("Enter a letter: ").lower()
-        if x in list:
-            print("Still have ",attempt,"attempt")
-            continue
-        else:
-            attempt=attempt-1
-            print("have",attempt,"attempt")
-        if attempt<=0:
-            print("done")
-            print('you lose')
+ans = (["_"] * len(secret_word))
+list=[]
+attempt=6
+while True:
+    print("answer:"," ".join(ans))
+    x = input("Enter a letter: ").lower()
+    if x in list:
+        print("Still have ",attempt,"attempt")
+        continue
+    else:
+        attempt=attempt-1
+        print("have",attempt,"attempt")
+    if attempt<=0:
+        print("done")
+        print('you lose')
+        break
+    list.append(x)
+    print(list)
+    if not x.isalpha():
+        print("invalid letter")
+        continue
+    if x in secret_word:
+        for i in range(len(secret_word)):
+            if secret_word[i] == x:       
+                ans[i] = x 
+        print("Good job")
+        if "_" not in ans:
+            print('Congratulations! You complete the task','"',secret_word,'"')
+            print('you win')
             break
-        list.append(x)
-        print(list)
-        if not x.isalpha():
-            print("invalid letter")
-            continue
-        if x in secret_word:
-            for i in range(len(secret_word)):
-                if secret_word[i] == x:       
-                    ans[i] = x 
-            print("Good job")
-            if "_" not in ans:
-                print('Congratulations! You complete the task','"',secret_word,'"')
-                print('you win')
-                break
-        else:
-            print("Wrong guess.Try again")
-main(secret_word)
+    else:
+        print("Wrong guess.Try again")
